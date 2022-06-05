fceea5b1bd34d38b37f6ab62b430859d8fa92776
Ilias El abbassi <iliaselabbassi@outlook.fr>
2022-06-05 20:52:12 +0200

    feat: begining of the front end, feat: created background, feat: connected to the local blockchain

--- src/component/App.js
+++ src/component/App.js
@@ -1,9 +1,45 @@
+import { useEffect, useState } from "react";
+
+import getBlockchain from '../ethereum.js';
+
+import "./style/bg.css"
 
 function App() {
+  const [address, setAddress] = useState(undefined)
+  const [diploma, setDiploma] = useState(undefined)
+
+
+  useEffect(() => {
+    const init = async () => {
+      const { signerAddress, diploma} = await getBlockchain();
+      console.log(signerAddress)
+      console.log(diploma)
+      setDiploma(diploma);
+      setAddress(signerAddress)
+    };
+    init()
+  }, []);
+
+  if(typeof diploma === 'undefined'){
+    return (
+      <div className="App">
+        <section className="bg">
+          <h1 className="bgh1">
+            Diplokma <br></br>
+            Please use an app like metamask<br></br>
+            to connect to the ethereum newtork
+          </h1>
+        </section>
+      </div>
+    )
+  }
   return (
     <div className="App">
-        <h1>Diplokma app</h1>
-        <h2>^^</h2>
+      <section className="bg">
+        <h1 className="bgh1">
+          Diplokma
+        </h1>
+      </section>
     </div>
   );
 }
